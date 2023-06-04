73c16b265388d4eeb7a8bff4ddcf20356a417b9d
smbirch <spencer.m.birch@gmail.com>
2023-06-04 12:07:01 -0700

    adjusting gunicorn

--- wsgi.py
+++ wsgi.py
@@ -2,3 +2,11 @@ from app import app
 
 if __name__ == "__main__":
     app.run()
+
+
+WorkingDirectory=/home/smbirch/pokepy_flask
+Environment="PATH=/home/smbirch/pokepy_flask/pokepyenv/bin"
+ExecStart=/home/smbirch/pokepy_flask/pokepy/bin/gunicorn --workers 3 --bind unix:myproject.sock -m 007 wsgi:app
+
+[Install]
+WantedBy=multi-user.target
